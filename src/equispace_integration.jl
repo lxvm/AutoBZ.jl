cc616ae4ecdf526d55d97891012b1d7a99c03567
Lorenzo X. Van Muñoz <lorenzo@vanmunoz.com>
2022-06-28 12:44:38 -0400

    speedup full bz equispace

--- src/equispace_integration.jl
+++ src/equispace_integration.jl
@@ -1,3 +1,5 @@
+export equispace_integration
+
 """
     Integrate a function on an equispace grid with the same number of grid
     points along each dimension
@@ -6,9 +8,11 @@ function equispace_integration(f, p::Int; rtol=sqrt(eps()))
     r = zero(SMatrix{3,3,ComplexF64})
     x = range(0.0, step=inv(p), length=p)
     for k in 1:p
+        @inbounds g = contract(f, x[k])
         for j in 1:p
+            @inbounds h = contract(g, x[j])
             for i in 1:p
-                @inbounds r += f(SVector(x[i], x[j], x[k]))
+                @inbounds r += h(SVector(x[i]))
             end
         end
     end
@@ -19,9 +23,11 @@ function equispace_integration(f::FourierSeries{3}, p::Int)
     r = Array{SMatrix{3,3,ComplexF64}}(undef, p^3)
     x = range(0.0, step=inv(p), length=p)
     for k in 1:p
+        @inbounds g = contract(f, x[k])
         for j in 1:p
+            @inbounds h = contract(g, x[j])
             for i in 1:p
-                @inbounds r[i,j,k] = f(SVector(x[i], x[j], x[k]))
+                @inbounds r[i,j,k] = h(SVector(x[i]))
             end
         end
     end
