53f3c78bb72683efac6b9b539530c6a15043f3b7
lxvm <lorenzo@vanmunoz.com>
2022-06-30 16:38:21 -0400

    more type specificity in hinv

--- src/inv.jl
+++ src/inv.jl
@@ -1,7 +1,7 @@
 """
 Calculate the inverse of a Hermitian matrix using its lower triangle
 """
-function hinv(A)
+function hinv(A::AbstractMatrix{T})::SMatrix{3,3,promote_type(Float64, T)} where {T<:Number}
     @inbounds x0 = A[5]*A[9] - abs2(A[6])
     @inbounds x1 = A[6]*conj(A[3]) - A[4]*A[9]
     @inbounds x2 = conj(A[2]*A[6]) - A[5]*conj(A[3])
