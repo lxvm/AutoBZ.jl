e0da173c88f0149b4c9de67200ff134662e1d23c
lxvm <lorenzo@vanmunoz.com>
2022-07-23 18:17:26 -0400

    separate limits in Applications

--- src/limits.jl
+++ src/limits.jl
@@ -1,64 +1,47 @@
-export lower, upper, IntegrationLimits, CubicLimits, TetrahedralLimits
-# have a separate file for integration limits
-# put the non-symmetrization code in other files
-abstract type IntegrationLimits end
+export TetrahedralLimits, FBZOpticalConductivity
 
 """
-    CubicLimits{N,T}
-A wrapper for `SVector{N,T}` that contains integration limit information for a cube
+    TetrahedralLimits(d, s)
+
+A parametrization of the integration limits for a tetrahedron generated from the
+automorphism group of the cube on [0,s]^d
 """
-struct CubicLimits{N,T} <: IntegrationLimits
-    x::SVector{N,T}
+struct TetrahedralLimits{d,N} <: IntegrationLimits{d}
+    x::SVector{N,Float64}
+    s::Float64
+    TetrahedralLimits{d}(x::SVector{N}, s) where {d,N} = new{d,N}(x, s)
 end
-CubicLimits() = CubicLimits(SVector())
-(l::CubicLimits)(x::Number) = CubicLimits(SVector(promote(x, l.x...)))
+TetrahedralLimits(d::Int, s) = TetrahedralLimits{d}(SVector{0,Float64}(), s)
+(l::TetrahedralLimits{d,N})(x::Number) where {d,N} = TetrahedralLimits{d-1}(vcat(x, l.x), l.s)
 
-lower(l::CubicLimits{0}) = 0.0
-lower(l::CubicLimits{1}) = 0.0
-lower(l::CubicLimits{2}) = 0.0
-upper(l::CubicLimits{0}) = 1.0
-upper(l::CubicLimits{1}) = 1.0
-upper(l::CubicLimits{2}) = 1.0
-rescale(::CubicLimits) = 1
+lower(::TetrahedralLimits) = 0.0
+upper(l::TetrahedralLimits{d,0}) where {d} = 0.5l.s
+upper(l::TetrahedralLimits) = first(l.x)
+rescale(::TetrahedralLimits{d}) where {d} = 2^d * factorial(d)
 
 """
-    TetrahedralLimits{N,T}
-A wrapper for `SVector{N,T}` that contains integration limit information for a tetrahedron
-"""
-struct TetrahedralLimits{N,T} <: IntegrationLimits
-    x::SVector{N,T}
-end
-TetrahedralLimits() = TetrahedralLimits(SVector()) 
-(l::TetrahedralLimits)(x::Number) = TetrahedralLimits(SVector(promote(x, l.x...)))
+    FBZOpticalConductivity
 
-lower(l::TetrahedralLimits{0}) = 0.0
-lower(l::TetrahedralLimits{1}) = 0.0
-lower(l::TetrahedralLimits{2}) = 0.0
-upper(l::TetrahedralLimits{0}) = 0.5
-upper(l::TetrahedralLimits{1}) = last(l.x)
-upper(l::TetrahedralLimits{2}) = first(l.x)
-rescale(::TetrahedralLimits) = 48
-
-"""
 These limits are designed for integrating over the cubic FBZ first, then over ω
 restricted to the domain where the fermi window is not converging to zero
 """
-struct FBZOpticalConductivity{N,T} <: IntegrationLimits
-    x::SVector{N,T}
+struct FBZOpticalConductivity{d,d_,Tl,Tu} <: IntegrationLimits{d}
+    c::CubicLimits{d_,Tl,Tu}
     Ω::Float64
     β::Float64
+    μ::Float64
     cutoff::Float64
+    function FBZOpticalConductivity(c::CubicLimits{d_,Tl,Tu}, Ω, β, μ, cutoff) where {d_,Tl,Tu}
+        new{d_+1,d_,Tl,Tu}(c, Ω, β, μ, cutoff)
+    end
 end 
 
-FBZOpticalConductivity(Ω, β, cutoff) = FBZOpticalConductivity(SVector(), Ω, β, cutoff)
-(l::FBZOpticalConductivity)(x::Number) = FBZOpticalConductivity(SVector(promote(x, l.x...)), l.Ω, l.β, l.cutoff)
+lower(l::FBZOpticalConductivity) = lower(l.c)
+lower(l::FBZOpticalConductivity{1}) = _lower_limit_fermi_window(l.Ω, l.β, l.μ, l.cutoff)
+upper(l::FBZOpticalConductivity) = upper(l.c)
+upper(l::FBZOpticalConductivity{1}) = _upper_limit_fermi_window(l.Ω, l.β, l.μ, l.cutoff)
+rescale(l::FBZOpticalConductivity) = rescale(l.c)
+(l::FBZOpticalConductivity)(x::Number) = FBZOpticalConductivity(l.c(x), l.Ω, l.β, l.μ, l.cutoff)
 
-lower(l::FBZOpticalConductivity{0}) = 0.0
-lower(l::FBZOpticalConductivity{1}) = 0.0
-lower(l::FBZOpticalConductivity{2}) = 0.0
-lower(l::FBZOpticalConductivity{3}) = -2.5max(l.Ω, l.β)
-upper(l::FBZOpticalConductivity{0}) = 1.0
-upper(l::FBZOpticalConductivity{1}) = 1.0
-upper(l::FBZOpticalConductivity{2}) = 1.0
-upper(l::FBZOpticalConductivity{3}) = 1.5max(l.Ω, l.β)
-rescale(::FBZOpticalConductivity) = 1
+_lower_limit_fermi_window(Ω, β, μ, cutoff) = μ-Ω-10inv(β)
+_upper_limit_fermi_window(Ω, β, μ, cutoff) = μ+10inv(β)
