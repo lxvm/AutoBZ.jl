aa1f7e7904e950f60cd83f080e0ddd897763fcce
lxvm <lorenzo@vanmunoz.com>
2022-07-24 10:40:22 -0400

    rename _iterated_integration to iterated_integration_

--- src/adaptive_integration.jl
+++ src/adaptive_integration.jl
@@ -7,34 +7,34 @@ Accepts a callback
 """
 iterated_integration(f, a, b; kwargs...) = iterated_integration(f, CubicLimits(a, b); kwargs...)
 function iterated_integration(f, L::IntegrationLimits; kwargs...)
-    int, err = _iterated_integration(f, L; kwargs...)
+    int, err = iterated_integration_(f, L; kwargs...)
     rescale(L)*int, err
 end
 
-_iterated_integration(f, L::IntegrationLimits{1}; callback=nothing, kwargs...) = hcubature(f, SVector(lower(L)), SVector(upper(L)); kwargs...)
-function _iterated_integration(f, L::IntegrationLimits; callback=thunk, kwargs...)
+iterated_integration_(f, L::IntegrationLimits{1}; callback=nothing, kwargs...) = hcubature(f, SVector(lower(L)), SVector(upper(L)); kwargs...)
+function iterated_integration_(f, L::IntegrationLimits; callback=thunk, kwargs...)
     hcubature(SVector(lower(L)), SVector(upper(L)); kwargs...) do x
         g = callback(f, x)
         L′ = L
-        first(_iterated_integration(g, L′(x); callback=callback, kwargs...))
+        first(iterated_integration_(g, L′(x); callback=callback, kwargs...))
     end
 end
 #= replacements with other quadrature routines
-_iterated_integration(f, L::IntegrationLimits{1}; callback=nothing, kwargs...) = hquadrature(f, lower(L), upper(L); kwargs...)
-function _iterated_integration(f, L::IntegrationLimits; callback=thunk, kwargs...) where {N}
+iterated_integration_(f, L::IntegrationLimits{1}; callback=nothing, kwargs...) = hquadrature(f, lower(L), upper(L); kwargs...)
+function iterated_integration_(f, L::IntegrationLimits; callback=thunk, kwargs...) where {N}
     hquadrature(lower(L), upper(L); kwargs...) do x
         g = callback(f, x)
         L′ = L
-        first(_iterated_integration(g, L′(x); callback=callback, kwargs...))
+        first(iterated_integration_(g, L′(x); callback=callback, kwargs...))
     end
 end
 
-_iterated_integration(f, L::IntegrationLimits{1}; callback=nothing, kwargs...) = quadgk(f, lower(L), upper(L); kwargs...)
-function _iterated_integration(f, L::IntegrationLimits; callback=thunk, kwargs...) where {N}
+iterated_integration_(f, L::IntegrationLimits{1}; callback=nothing, kwargs...) = quadgk(f, lower(L), upper(L); kwargs...)
+function iterated_integration_(f, L::IntegrationLimits; callback=thunk, kwargs...) where {N}
     quadgk(lower(L), upper(L); kwargs...) do x
         g = callback(f, x)
         L′ = L
-        first(_iterated_integration(g, L′(x); callback=callback, kwargs...))
+        first(iterated_integration_(g, L′(x); callback=callback, kwargs...))
     end
 end
 =#
