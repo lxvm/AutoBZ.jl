33f4c95a70fdbb89b13ee90ffe25aa3521b0d8b7
lxvm <lorenzo@vanmunoz.com>
2022-08-10 21:07:19 -0400

    draft self energy type

---
+++ src/self_energies.jl
@@ -0,0 +1,11 @@
+"""
+    AbstractSelfEnergy
+
+Interface: instances are callable and return a matrix as a function of frequency.
+"""
+abstract type AbstractSelfEnergy end
+
+struct EtaEnergy{T<:Real} <: AbstractSelfEnergy
+    η::T
+end
+(Σ::EtaEnergy)(::Number) = complex(zero(Σ.η), Σ.η)*I
