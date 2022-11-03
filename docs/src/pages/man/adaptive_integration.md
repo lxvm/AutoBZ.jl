2c870468285e2226dd1dde6f932ac2c6654e93a7
lxvm <lorenzo@vanmunoz.com>
2022-11-03 19:11:43 -0400

    update adaptive interface

--- docs/src/pages/man/adaptive_integration.md
+++ docs/src/pages/man/adaptive_integration.md
@@ -9,8 +9,11 @@ iterated_integration
 
 ## Customization
 
+The methods below can be extended/customized to integrand types
+
 ```@docs
-thunk
+iterated_tol_update
+iterated_pre_eval
+AutoBZ.thunk
 AutoBZ.ThunkIntegrand
-AutoBZ.default_error_callback
 ```
--- src/adaptive_integration.jl
+++ src/adaptive_integration.jl
@@ -1,5 +1,6 @@
-export tree_integration, iterated_integration
-# TODO: rename callback to pre_eval
+export tree_integration, iterated_integration,
+    iterated_tol_update, iterated_pre_eval
+
 """
     tree_integration(f, a, b)
     tree_integration(f, ::CubicLimits)
@@ -9,31 +10,39 @@ Calls `HCubature` to perform multi-dimensional integration of `f` over a cube.
 tree_integration(f, a, b; callback=nothing, kwargs...) = hcubature(f, a, b; kwargs...)
 tree_integration(f, c::CubicLimits; kwargs...) = tree_integration(f, c.l, c.u; kwargs...)
 
-"Choose a new set of error tolerances for the next inner integral."
-default_error_callback(atol, rtol) = (atol, rtol)
+"""
+    iterated_tol_update(f, l, atol, rtol)
+
+Choose a new set of error tolerances for the next inner integral. By default
+returns `(atol, rtol)` unchanged.
+"""
+iterated_tol_update(f, l, atol, rtol) = (atol, rtol)
+
+"""
+    iterated_pre_eval(f, x)
+
+Perform a precomputation
+"""
+iterated_pre_eval(f, x) = thunk(f, x)
 
 """
     iterated_integration(f, ::IntegrationLimits)
 
 Calls `HCubature` to perform iterated 1D integration of `f` over a domain
 parametrized by `IntegrationLimits`.
-Accepts a callback function whose arguments are `f` and the evaluation point,
-`x`, as a keyword argument. The callback can return a modified integrand to the
-next inner integral, but the default is `thunk` which delays the computation to
-the innermost integral.
 """
 iterated_integration(f, a, b; kwargs...) = iterated_integration(f, CubicLimits(a, b); kwargs...)
-function iterated_integration(f, l::IntegrationLimits; callback=thunk, atol=0.0, rtol=sqrt(eps()), error_callback=default_error_callback, kwargs...)
-    int, err = iterated_integration_(f, l, callback, atol/nsyms(l), rtol, error_callback; kwargs...)
+function iterated_integration(f, l::IntegrationLimits;atol=0.0, rtol=sqrt(eps()), kwargs...)
+    int, err = iterated_integration_(f, l, atol/nsyms(l), rtol; kwargs...)
     symmetrize(l, int, err)
 end
 
-function iterated_integration_(f, l::IntegrationLimits{1}, callback, atol, rtol, error_callback; kwargs...) 
+function iterated_integration_(f, l::IntegrationLimits{1}, atol, rtol; kwargs...) 
     hcubature(f, SVector(lower(l)), SVector(upper(l)); atol=atol, rtol=rtol, kwargs...)
 end
-function iterated_integration_(f, l::IntegrationLimits, callback, atol, rtol, error_callback; kwargs...)
+function iterated_integration_(f, l::IntegrationLimits, atol, rtol; kwargs...)
     hcubature(SVector(lower(l)), SVector(upper(l)); atol=atol, rtol=rtol, kwargs...) do x
-        first(iterated_integration_(callback(f, x), l(x), callback, error_callback(atol, rtol)..., error_callback; kwargs...))
+        first(iterated_integration_(iterated_pre_eval(f, x), l(x), iterated_tol_update(f, l, atol, rtol)...; kwargs...))
     end
 end
 #= replacements with other quadrature routines
