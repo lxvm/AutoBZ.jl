0c411b47a96238cc422d2b73dd74e0542b47c38e
lxvm <lorenzo@vanmunoz.com>
2022-11-11 18:20:33 -0500

    adopt fourier3d in wannierio

--- src/wannier90io.jl
+++ src/wannier90io.jl
@@ -54,34 +54,39 @@ keyword `compact` to specify:
 """
 function load_hamiltonian(filename; period=1.0, compact=:N)
     date_time, num_wann, nrpts, degen, irvec, C_ = parse_hamiltonian(filename)
-    s = Int(cbrt(nrpts))
-    r = Int((s-1)/2)
-    C = load_coefficients(Val{compact}(), num_wann, s, r, irvec, C_)[1]
-    FourierSeries(OffsetArray(C, -r:r, -r:r, -r:r), to_3period(period))
+    C = load_coefficients(Val{compact}(), num_wann, irvec, C_)[1]
+    FourierSeries3D(C, to_3period(period))
 end
 
-to_3period(x::T) where {T} = fill(x, SVector{3,T})
-to_3period(x::SVector{3}) = identity(x)
+to_3period(x::Real) = to_3period(convert(Float64, x))
+to_3period(x::Float64) = (x,x,x)
+to_3period(x::NTuple{3,Float64}) = x
 
-load_coefficients(compact, num_wann, s, r, irvec, cs...) = load_coefficients(compact, num_wann, s, r, irvec, cs)
-@generated function load_coefficients(compact, num_wann, s, r, irvec, cs::NTuple{N}) where N
+load_coefficients(compact, num_wann, irvec, cs...) = load_coefficients(compact, num_wann, irvec, cs)
+@generated function load_coefficients(compact, num_wann, irvec, cs::NTuple{N}) where N
+    T_full = :(SMatrix{num_wann,num_wann,ComplexF64,num_wann^2})
+    T_compact = :(SHermitianCompact{num_wann,ComplexF64,StaticArrays.triangularnumber(num_wann)})
     if compact === Val{:N}
-        T = :(SMatrix{num_wann,num_wann,ComplexF64,num_wann^2})
-        expr = :(c[i])
+        T = T_full; expr = :(c[i])
     elseif compact === Val{:L}
-        T = :(SHermitianCompact{num_wann,ComplexF64,StaticArrays.triangularnumber(num_wann)})
-        expr = :($T(c[i]))
+        T = T_compact; expr = :($T(c[i]))
     elseif compact === Val{:U}
-        T = :(SHermitianCompact{num_wann,ComplexF64,StaticArrays.triangularnumber(num_wann)})
-        expr = :($T(c[i]'))
+        T = T_compact; expr = :($T(c[i]'))
     elseif compact === Val{:S}
-        T = :(SHermitianCompact{num_wann,ComplexF64,StaticArrays.triangularnumber(num_wann)})
-        expr = :($T(0.5*(c[i]+c[i]')))
+        T = T_compact; expr = :($T(0.5*(c[i]+c[i]')))
     end
 quote
-    Cs = Base.Cartesian.@ntuple $N _ -> Array{$T,3}(undef, s,s,s)
-    for (i, idx) in enumerate(irvec)
-        idx_ = CartesianIndex((idx .+ (r+1))...)
+    nmodes = zeros(Int, 3)
+    for idx in irvec
+        @inbounds for i in 1:3
+            if (n = abs(idx[i])) > nmodes[i]
+                nmodes[i] = n
+            end
+        end
+    end
+    Cs = Base.Cartesian.@ntuple $N _ -> zeros($T, (2nmodes .+ 1)...)
+    for (i,idx) in enumerate(irvec)
+        idx_ = CartesianIndex((idx .+ nmodes .+ 1)...)
         for (j,c) in enumerate(cs)
             Cs[j][idx_] = $expr
         end
@@ -103,9 +108,10 @@ parse_position_operator(filename) = open(filename) do file
     num_wann = parse(Int, readline(file))
     nrpts = parse(Int, readline(file))
 
-    X = Vector{SMatrix{num_wann,num_wann,ComplexF64,num_wann^2}}(undef, nrpts)
-    Y = Vector{SMatrix{num_wann,num_wann,ComplexF64,num_wann^2}}(undef, nrpts)
-    Z = Vector{SMatrix{num_wann,num_wann,ComplexF64,num_wann^2}}(undef, nrpts)
+    T = SMatrix{num_wann,num_wann,ComplexF64,num_wann^2}
+    X = Vector{T}(undef, nrpts)
+    Y = Vector{T}(undef, nrpts)
+    Z = Vector{T}(undef, nrpts)
     irvec = Vector{SVector{3,Int}}(undef, nrpts)
     x = Matrix{ComplexF64}(undef, num_wann, num_wann)
     y = Matrix{ComplexF64}(undef, num_wann, num_wann)
@@ -116,19 +122,19 @@ parse_position_operator(filename) = open(filename) do file
             irvec[k] = SVector{3,Int}(parse(Int, col[1]), parse(Int, col[2]), parse(Int, col[3]))
             m = parse(Int, col[4])
             n = parse(Int, col[5])
-            re = parse(Float64, col[6])
-            im = parse(Float64, col[7])
-            x[m,n] = complex(re, im)
-            re = parse(Float64, col[8])
-            im = parse(Float64, col[9])
-            y[m,n] = complex(re, im)
-            re = parse(Float64, col[10])
-            im = parse(Float64, col[11])
-            z[m,n] = complex(re, im)
+            re_x = parse(Float64, col[6])
+            im_x = parse(Float64, col[7])
+            x[m,n] = complex(re_x, im_x)
+            re_y = parse(Float64, col[8])
+            im_y = parse(Float64, col[9])
+            y[m,n] = complex(re_y, im_y)
+            re_z = parse(Float64, col[10])
+            im_z = parse(Float64, col[11])
+            z[m,n] = complex(re_z, im_z)
         end
-        X[k] = SMatrix{num_wann,num_wann,ComplexF64,num_wann^2}(x)
-        Y[k] = SMatrix{num_wann,num_wann,ComplexF64,num_wann^2}(y)
-        Z[k] = SMatrix{num_wann,num_wann,ComplexF64,num_wann^2}(z)
+        X[k] = T(x)
+        Y[k] = T(y)
+        Z[k] = T(z)
     end
     date_time, num_wann, nrpts, irvec, X, Y, Z
 end
@@ -147,15 +153,25 @@ Fourier coefficients in compact form, use the keyword `compact` to specify:
 - `:L`: store the lower triangle of the coefficients
 - `:U`: store the upper triangle of the coefficients
 - `:S`: store the lower triangle of the symmetrized coefficients, `(c+c')/2`
+Note that in some cases the coefficients are not Hermitian even though the
+values of the series are.
 """
 function load_position_operator(filename; period=1.0, compact=:N)
     date_time, num_wann, nrpts, irvec, X_, Y_, Z_ = parse_position_operator(filename)
-    s = Int(cbrt(nrpts))
-    r = Int((s-1)/2)
-    X, Y, Z = load_coefficients(Val{compact}(), num_wann, s, r, irvec, X_, Y_, Z_)
+    X, Y, Z = load_coefficients(Val{compact}(), num_wann, irvec, X_, Y_, Z_)
     periods = to_3period(period)
-    FX = FourierSeries(OffsetArray(X, -r:r, -r:r, -r:r), periods)
-    FY = FourierSeries(OffsetArray(Y, -r:r, -r:r, -r:r), periods)
-    FZ = FourierSeries(OffsetArray(Z, -r:r, -r:r, -r:r), periods)
-    ManyFourierSeries(FX, FY, FZ)
+    FX = FourierSeries3D(X, periods)
+    FY = FourierSeries3D(Y, periods)
+    FZ = FourierSeries3D(Z, periods)
+    FX, FY, FZ
+end
+
+function load_hamiltonian_velocities(f_hamiltonian; period=1.0, compact=:N)
+    H = load_hamiltonian(f_hamiltonian; period=period, compact=compact)
+    BandEnergyVelocity3D(H)
+end
+function load_hamiltonian_velocities(f_hamiltonian, f_pos_op; period=1.0, compact=:N)
+    H = load_hamiltonian(f_hamiltonian; period=period, compact=compact)
+    Ax, Ay, Az = load_position_operator(f_pos_op; period=period, compact=compact)
+    BandEnergyBerryVelocity3D(H, Ax, Ay, Az)
 end
