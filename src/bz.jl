22fb499a09fa0570256dcf0fa9d060816a125354
lxvm <lorenzo@vanmunoz.com>
2023-01-26 10:36:17 -0500

    more options for ibz

--- src/bz.jl
+++ src/bz.jl
@@ -40,18 +40,20 @@ Return an identity matrix.
 symmetries(::FullBZ) = tuple(I)
 Base.eltype(::Type{FullBZ{d}}) where d = Float64
 
-struct IrreducibleBZ{d,L<:IntegrationLimits{d},d_} <: AbstractBZ{d}
+struct IrreducibleBZ{d,L<:IntegrationLimits{d},S,d_} <: AbstractBZ{d}
     a::Vector{SVector{d_,Float64}}
     b::Vector{SVector{d_,Float64}}
     lims::L
+    syms::S
 end
 
-(l::IrreducibleBZ)(x) = IrreducibleBZ(l.a, l.b, l.lims(x))
+IrreducibleBZ(a,b,lims) = IrreducibleBZ(a,b,lims,symmetries(lims))
+(l::IrreducibleBZ)(x) = IrreducibleBZ(l.a, l.b, l.lims(x),l.syms)
 limits(l::IrreducibleBZ, dim) = limits(l.lims, dim)
 box(l::IrreducibleBZ) = box(l.lims)
-nsyms(l::IrreducibleBZ) = nsyms(l.lims)
-symmetries(l::IrreducibleBZ) = symmetries(l.lims)
-Base.eltype(::Type{IrreducibleBZ{d,L,d_}}) where {d,L,d_} = eltype(L)
+nsyms(l::IrreducibleBZ) = length(l.syms)
+symmetries(l::IrreducibleBZ) = l.syms
+Base.eltype(::Type{IrreducibleBZ{d,L,S,d_}}) where {d,L,S,d_} = eltype(L)
 
 struct AffineTransform{d,T,L}
     R::SMatrix{d,d,T,L}
